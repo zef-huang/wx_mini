841cd03125838642ba7d52c4a38dc27a18061493
huangzefeng <737244264@qq.com>
2022-09-17 19:55:37 +0800

    遍历对象数组

--- miniprogram/pages/demo/demo.ts
+++ miniprogram/pages/demo/demo.ts
@@ -1,9 +1,10 @@
 // pages/demo/demo.ts
 Page({
     data: {
-      arr: ["beijing", "shanghai", "guangzhou", "shenzhen"]
+      arr: ["beijing", "shanghai", "guangzhou", "shenzhen"],
+      objArr: [{name: "zjl", age: 12}]
     },
-    
+
     handleClick () {
       console.log("我被点击了")
     }
